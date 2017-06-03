1549f7b0028d8870afe87083c3bb4be00668ec87
garyemerson <gmohammadioun@gmail.com>
2017-06-03 12:58:08 -0700

    add city selection/dropdown

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -36,13 +36,31 @@ class PreviewBar extends React.Component<any, any> {
 class CityInput extends React.Component<any, any> {
   constructor(props: any) {
     super(props)
-    this.handleBlur = this.handleBlur.bind(this)
-    this.handleFocus = this.handleFocus.bind(this)
+    let cities = fs.readFileSync("/Users/Garrett/workspaces/centroid/cities", "utf8").split("\n")
+    let options = {
+      shouldSort: true,
+      includeMatches: true,
+      threshold: 0.6,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: [
+        "title",
+        "author.firstName"
+      ]
+    };
+    this.state = {
+      inputText: "",
+      cities: cities,
+      // fz: new Fuse(cities, options),
+    }
   }
 
   style = {
-    width: "100px",
-    height: "100px",
+    width: "200px",
+    height: "50px",
+    padding: "5px",
   }
   inputStyle = {
     width: "100%",
@@ -53,52 +71,68 @@ class CityInput extends React.Component<any, any> {
     margin: 0,
     padding: 0,
     display: "none",
+    position: "relative",
+    overflowY: "scroll",
+    height: "250px",
+    zIndex: 1,
   }
   liStyle = {
     borderLeft: "1px solid black",
-    borderRight: "1px solid black",
+    // borderRight: "1px solid black",
     borderBottom: "1px solid black",
   }
 
   handleFocus() {
     console.log("focus")
-    console.log("this is " + this)
+    this.ulStyle = { ...this.ulStyle }
     this.ulStyle.display = "block"
     render()
   }
 
   handleBlur() {
     console.log("blur")
-    console.log("this is " + this)
+    this.ulStyle = { ...this.ulStyle }
     this.ulStyle.display = "none"
     render()
   }
 
+  handleChange(event: any) {
+    console.log("change event triggered with key '%s'", event.target.value)
+    this.setState({
+      inputText: event.target.value
+    });
+  }
+
   render() {
+    let ulElems: JSX.Element[] = [];
+    for (let i = 0; i < this.state.cities.length; i++) {
+      ulElems.push(<li style={this.liStyle}>{this.state.cities[i]}{this.state.inputText}</li>)
+    }
+
     return <div style={this.style}>
-       <input style={this.inputStyle} type="text" name="city" placeholder="city" onFocus={this.handleFocus} onBlur={this.handleBlur}></input>
-       <ul style={this.ulStyle}>
-         <li style={this.liStyle}>foo</li>
-         <li style={this.liStyle}>bar</li>
-         <li style={this.liStyle}>baz</li>
-       </ul>
-     </div>
+      <input value={this.state.inputText} style={this.inputStyle} type="text" name="city"
+        placeholder="city"onFocus={this.handleFocus.bind(this)}
+        onBlur={this.handleBlur.bind(this)} onChange={this.handleChange.bind(this)}/>
+      <ul style={this.ulStyle}>
+        {ulElems}
+      </ul>
+    </div>
   }
 }
 
 class Preview extends React.Component<any, any> {
   constructor() {
     super()
-    var cities = fs.readFileSync("/Users/Garrett/workspaces/centroid/cities", "utf8").split("\n")
+    let cities = fs.readFileSync("/Users/Garrett/workspaces/centroid/cities", "utf8").split("\n")
     this.city = cities[Math.floor(Math.random() * cities.length)]
-    this.city = this.city.replace(", ", ",")
-    this.city = this.city.replace(" ", "+")
+    this.city = this.city.replace(/, /g , ",")
+    this.city = this.city.replace(/ /g, "+")
     console.log("city is " + this.city)
 
-    var key = fs.readFileSync('/Users/Garrett/workspaces/centroid/api_key')
+    let key = fs.readFileSync('/Users/Garrett/workspaces/centroid/api_key')
     // console.log("api key is " + key)
     this.img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+ this.city + 
-      "&zoom=5&size=640x400&path=weight:3%7Ccolor:blue%7Cenc:{coaHnetiVjM??_SkM??~R" + 
+      "&zoom=5&size=500x300&path=weight:3%7Ccolor:blue%7Cenc:{coaHnetiVjM??_SkM??~R" + 
       "&markers=color:red%7C" + this.city + 
       "&key=" + key
 
@@ -108,7 +142,7 @@ class Preview extends React.Component<any, any> {
   img_url: string
   imgStyle = {
     width: "300px",
-    // height: "150px",
+    height: "180px",
     background: "#aacbff",
     border: "1px solid black",
     // verticalAlign: "middle",
@@ -131,7 +165,7 @@ class Preview extends React.Component<any, any> {
 
 function render() {
   ReactDOM.render(
-    <PreviewBar />,
+    <PreviewBar/>,
     document.getElementById('root')
   )
 }
